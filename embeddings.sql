911a7ecb58ff627ef4849faf0a99081841811dec
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2025-03-31 14:37:11 -0500

    removed extraneous sql

--- embeddings.sql
+++ embeddings.sql
@@ -1,12 +1,3 @@
-CREATE TABLE embeddings (
-    "EmbeddingId" INTEGER PRIMARY KEY, 
-    "PatientId" VARCHAR(500),
-    "SessionNumber" Integer,    
-    "embeddings" VARCHAR(5000),
-    "model" TEXT,
-    "CreateDate" DATETIME
-); 
-
 CREATE TABLE subsections (
     "SectionID" INTEGER PRIMARY KEY, 
     "PatientId" VARCHAR(500),
